76bec0f1fadf55626bc05c23a7eeb07461acce68
dimedrolex <dimaxfronin@gmail.com>
2022-07-21 16:41:55 +0300

    first HTML on the page

--- mi3/main/views.py
+++ mi3/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("<h3>Xiaomi Mi Electric Scooter 3</h3>")
+    return render(request, 'main/index.html')
+
+
+def about(request):
+    return HttpResponse("<h2>Страница про нас</h2>")
