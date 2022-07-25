91466db827777f0f0fdb1f33df26fa01e46147f6
dimedrolex <dimaxfronin@gmail.com>
2022-07-25 12:19:54 +0300

    db creation & migration

--- mi3/news/models.py
+++ mi3/news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=60)
+    annons = models.CharField('Анонс', max_length=250)
+    # image = models.ImageField(null=True, blank=True, upload_to="images/")
+    text = models.TextField('Статья')
+    post_date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
