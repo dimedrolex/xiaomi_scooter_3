ce82939f24a808f993ddac2ca1067d1b0eadfa31
dimedrolex <dimaxfronin@gmail.com>
2022-07-27 19:54:59 +0300

    dynamic news pages

--- mi3/news/urls.py
+++ mi3/news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.news_general, name='news_general'),
     path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news_detail')
 ]
--- mi3/news/views.py
+++ mi3/news/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-# from django.views.generic import DetalView
+from django.views.generic import DetailView
 
 
 def news_general(request):
     news = Articles.objects.all()
     return render(request, 'news/news.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details.html'
+    context_object_name = 'article_gen'
 
 
 def create(request):
